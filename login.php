90f283b0bf3d3115522f2f98601af9c975bb6252
Gosick96 <jusem11@hotmail.com>
2018-10-02 16:55:09 -0500

    comentarios y expliaciones de logica

--- login.php
+++ login.php
@@ -1,19 +1,23 @@
 <?php session_start();
+	// COMPRUEBA SI HAY UNA SESIÓN ↓↓↓
 	if (isset($_SESSION['usuario'])) {
 		header('location: index.php');
 	} 
 	$errores = '';
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		// CAPTURA DE DATOS DEL FORMULARIO ↓↓↓
 		$usuario = filter_var(strtolower($_POST['user']), FILTER_SANITIZE_EMAIL);
 		$password = $_POST['password'];
 		$password = hash('sha512', $password);
 
+		// CONEXION CON LA BASE DE DATOS ↓↓↓
 		try {
 			$conexion = new PDO('mysql:host=localhost;dbname=prueba_login', 'root', '');
 		} catch (PDOException $e) {
 			die();
 		}
 
+		// CONSULTA A LA BASE DE DATOS COMPARANDO LOS CAMPOS DE USUARIO Y CONTRASEÑA CON LA BASE DE DATOS ↓↓↓
 		$statement = $conexion->prepare(
 			'SELECT * FROM usuarios WHERE  correo = :user AND password = :password'
 		);
@@ -24,6 +28,7 @@
 		$resultado = $statement->fetch();
 		var_dump($resultado);
 
+		// SI LOS CAMPOS NO SON IGUALES ↓↓↓
 		if ($resultado != false) {
 			$_SESSION['usuario'] = $usuario;
 			header('location: index.php');
--- register.php
+++ register.php
@@ -1,8 +1,12 @@
 <?php session_start();
+
+	// COMPRUEBA SI HAY UNA SESION INICIADA ↓↓↓
 	if (isset($_SESSION['usuario'])) {
 		header('location: index.php');
 	} 
+	// COMPRUEBA SI EL METODO DE ENVIO ES POST ↓↓↓
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		// GUARDANDO LOS DATOS DEL FORMULARIO EN VARIABLES ↓↓↓
 		$name = filter_var(strtolower($_POST['name']), FILTER_SANITIZE_STRING);
 		$lastname = filter_var(strtolower($_POST['apellido']), FILTER_SANITIZE_STRING);
 		$mail = $_POST['mail'];
@@ -11,14 +15,17 @@
 
 		$errores = '';
 
+		// COMPRUEBA SI EL FORMULARIO TIENE LOS CAMPOS LLENOS ↓↓↓
 		if(empty($name) or empty($lastname) or empty($mail) or empty($password) or empty($password2)) {
 			$errores .= '<li>Ingrese todos los datos</li>';
 		} else {
+			// CONEXION A BASE DE DATOS ↓↓↓
 			try {
 				$conexion = new PDO('mysql:host=localhost;dbname=prueba_login', 'root', '');
 			} catch (PDOException $e) {
 				die();
 			}
+			// COMPARA EL VALOR DE INPUT CON LA BASE DE DATOS ↓↓↓
 			$statement = $conexion->prepare('SELECT * FROM usuarios WHERE correo = :mail LIMIT 1');
 			$statement->execute(array(
 				':mail' => $mail
@@ -29,6 +36,7 @@
 				$errores .= '<li> El correo ingresado ya existe </li>';				
 			}
 
+			// ENCRIPTADO DE CONTRASEÑA PARA LA BASE DE DATOS ↓↓↓
 			$password = hash('sha512', $password);
 			$password2 = hash('sha512', $password2);
 
@@ -36,6 +44,7 @@
 				$errores .= '<li> Las contraseñas son diferentes </li>';
 			}
 
+			// COMPRUEBA QUE EL CORREO SEA VALIDO ↓↓↓
 			if (!empty($mail)) {
 				$mail = filter_var($mail, FILTER_SANITIZE_EMAIL);
 
@@ -46,6 +55,7 @@
 				$errores .= '<li> Ingrese un correo </li>';
 			}
 		}
+		// SI  NO HAY ERRORES SE INSERTAN LOS DATOS EN LA BASE DE DATOS ↓↓↓
 		if ($errores == '') {
 			$statement = $conexion->prepare('INSERT INTO usuarios (id, nombre, apellido, correo, password) VALUES (NULL, :name, :apellido, :mail, :pass1)');
 			$statement->execute(array(
