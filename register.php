928294a85a072ca3b2096b2c6b565dcf029cc1f8
Gosick96 <jusem11@hotmail.com>
2018-10-02 16:59:55 -0500

    comentarios y expliaciones de logica

--- register.php
+++ register.php
@@ -36,7 +36,7 @@
 				$errores .= '<li> El correo ingresado ya existe </li>';				
 			}
 
-			// ENCRIPTADO DE CONTRASEÑA PARA LA BASE DE DATOS ↓↓↓
+			// ENCRIPTANDO Y COMPARANDO CONTRASEÑAS LA BASE DE DATOS ↓↓↓
 			$password = hash('sha512', $password);
 			$password2 = hash('sha512', $password2);
 
